d068801da23031b5474a6547bcf701b42b110580
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-11 16:15:25 +0200

    add conditions

--- main.js
+++ main.js
@@ -15,4 +15,24 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
  */
 
+const prezzoBiglietto = 0.21;
+const prezzoTotale = (numeroChilometri * prezzoBiglietto).toFixed(2);
+console.log("il prezzo totale è: " + prezzoTotale);
+const ageUnder = 18;
+const ageOver = 65;
+
+if (userAge < ageUnder) {
+    /* console.log("l'utente  è minorenne ed ha diritto ad un 20% di sconto"); */
+    const bigliettoUnder = (prezzoTotale * (80/100)).toFixed(2);
+    console.log("il prezzo del biglietto dell'utente minorenne è: " + bigliettoUnder + "€");
+
+} else if (userAge > ageOver) {
+    /* console.log("l'utente  è nella fascia over 65 ed ha diritto ad un 40% di sconto"); */
+    const bigliettoOver = (prezzoTotale * (60/100)).toFixed(2);
+    console.log("il prezzo del biglietto dell'utente della fascia over 65 è: " + bigliettoOver + "€");
+
+} else {    
+    console.log("il prezzo del biglietto regolare è: " + prezzoTotale + "€");
+}
+
 // L'output del prezzo finale va messo fuori con massimo due decimali, per indicare centesimi sul prezzo.
