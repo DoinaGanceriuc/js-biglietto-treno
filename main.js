918281ba77dc55298c9545552145e6ac7cddf629
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-11 15:37:54 +0200

    ask user km and age

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 
+
+const numeroChilometri = Number(prompt("Indicare il numero dei chilometri che si intende percorrere"));
+console.log("il numero dei chilometri è: " + numeroChilometri);
+
+const userAge = Number(prompt("Quanti anni hai?"));
+console.log("l'età dell'utente è: " + userAge);
+
+
+
 /* Calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni 
